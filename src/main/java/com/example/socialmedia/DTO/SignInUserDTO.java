eeaf061e582f408874797ffd8b8a33361c15fc10
LyMinh85 <mlym8565@gmail.com>
2023-11-17 15:38:58 +0700

    Add sign-up feature.

--- src/main/java/com/example/socialmedia/DTO/SignInUserDTO.java
+++ src/main/java/com/example/socialmedia/DTO/SignInUserDTO.java
@@ -1,4 +1,14 @@
 package com.example.socialmedia.DTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 public class SignInUserDTO {
+    @NotEmpty(message = "Email không được để trống")
+    @Email(message = "Không đúng định dạng email")
+    private String email;
+
+    @NotEmpty(message = "Password không được để trống")
+    private String password;
 }

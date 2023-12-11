a51822d5f30fca27c11952590f2c1d46beeb03e9
LyMinh85 <mlym8565@gmail.com>
2023-12-11 22:28:31 +0700

    Update db.

--- create_database.sql
+++ create_database.sql
@@ -29,7 +29,7 @@ CREATE TABLE `avatar_image` (
                                 `avatar_image_id` int unsigned NOT NULL AUTO_INCREMENT,
                                 PRIMARY KEY (`avatar_image_id`),
                                 CONSTRAINT `avatar_image_image_image_id_fk` FOREIGN KEY (`avatar_image_id`) REFERENCES `image` (`image_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,6 +38,7 @@ CREATE TABLE `avatar_image` (
 
 LOCK TABLES `avatar_image` WRITE;
 /*!40000 ALTER TABLE `avatar_image` DISABLE KEYS */;
+INSERT INTO `avatar_image` VALUES (1),(2),(3);
 /*!40000 ALTER TABLE `avatar_image` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,7 +63,7 @@ CREATE TABLE `comment` (
                            CONSTRAINT `comment_comment_comment_id_fk` FOREIGN KEY (`parent_comment_id`) REFERENCES `comment` (`comment_id`),
                            CONSTRAINT `comment_post_post_id_fk` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`),
                            CONSTRAINT `comment_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,6 +72,7 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (14,'Yeah','2023-12-10 22:42:42',7,4,NULL),(15,'Oh yeah','2023-12-10 23:05:25',7,4,NULL),(16,'haha','2023-12-10 23:09:17',7,6,NULL);
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,7 +94,7 @@ CREATE TABLE `friendship` (
                               KEY `friendship_user_user_id_fk2` (`user2_id`),
                               CONSTRAINT `friendship_user_user_id_fk` FOREIGN KEY (`user1_id`) REFERENCES `user` (`user_id`),
                               CONSTRAINT `friendship_user_user_id_fk2` FOREIGN KEY (`user2_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,7 +103,7 @@ CREATE TABLE `friendship` (
 
 LOCK TABLES `friendship` WRITE;
 /*!40000 ALTER TABLE `friendship` DISABLE KEYS */;
-INSERT INTO `friendship` VALUES (1,1,2,'Accepted','2023-11-08 16:24:07');
+INSERT INTO `friendship` VALUES (6,7,5,'ACCEPTED','2023-12-09 16:11:30'),(7,7,6,'PENDING','2023-12-10 09:48:23'),(8,8,7,'ACCEPTED','2023-12-10 10:17:21');
 /*!40000 ALTER TABLE `friendship` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,11 +116,11 @@ DROP TABLE IF EXISTS `image`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `image` (
                          `image_id` int unsigned NOT NULL AUTO_INCREMENT,
-                         `public_id` varchar(200) NOT NULL,
+                         `public_id` varchar(200) DEFAULT NULL,
                          `url` varchar(300) NOT NULL,
                          `upload_on` datetime NOT NULL,
                          PRIMARY KEY (`image_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,6 +129,7 @@ CREATE TABLE `image` (
 
 LOCK TABLES `image` WRITE;
 /*!40000 ALTER TABLE `image` DISABLE KEYS */;
+INSERT INTO `image` VALUES (1,NULL,'/images00068-3037495260.jpeg','2023-12-06 11:49:34'),(2,NULL,'https://res.cloudinary.com/du163r961/image/upload/v1701841064/social-media/IMG_7911_z3nvws.jpg','2023-12-06 12:38:08'),(3,NULL,'https://res.cloudinary.com/du163r961/image/upload/v1702178126/social-media/IMG_8414_rz6ynq.jpg','2023-12-10 10:15:50');
 /*!40000 ALTER TABLE `image` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,7 +150,7 @@ CREATE TABLE `likes` (
                          KEY `likes_user_user_id_fk` (`user_id`),
                          CONSTRAINT `likes_post_post_id_fk` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`),
                          CONSTRAINT `likes_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -156,7 +159,7 @@ CREATE TABLE `likes` (
 
 LOCK TABLES `likes` WRITE;
 /*!40000 ALTER TABLE `likes` DISABLE KEYS */;
-INSERT INTO `likes` VALUES (1,2,1,'2023-11-08 16:43:21');
+INSERT INTO `likes` VALUES (8,7,4,'2023-12-10 23:08:51'),(11,7,6,'2023-12-10 23:09:06'),(12,7,3,'2023-12-10 23:09:10');
 /*!40000 ALTER TABLE `likes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -177,7 +180,7 @@ CREATE TABLE `post` (
                         PRIMARY KEY (`post_id`),
                         KEY `post_user_user_id_fk` (`user_id`),
                         CONSTRAINT `post_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -186,7 +189,7 @@ CREATE TABLE `post` (
 
 LOCK TABLES `post` WRITE;
 /*!40000 ALTER TABLE `post` DISABLE KEYS */;
-INSERT INTO `post` VALUES (1,'First post','2023-11-08 16:39:45',0,0,1);
+INSERT INTO `post` VALUES (2,'Something new','2023-12-05 13:22:35',0,0,5),(3,'Something incredible','2023-12-05 13:23:33',1,0,5),(4,'I am Quoc Nam!','2023-12-06 13:14:42',1,2,6),(5,'Hello everyone.','2023-12-08 22:17:27',0,0,7),(6,'ehe','2023-12-10 11:59:19',1,1,7);
 /*!40000 ALTER TABLE `post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -237,7 +240,7 @@ CREATE TABLE `user` (
                         PRIMARY KEY (`user_id`),
                         KEY `user_avatar_image_avatar_image_id_fk` (`avatar_image_id`),
                         CONSTRAINT `user_avatar_image_avatar_image_id_fk` FOREIGN KEY (`avatar_image_id`) REFERENCES `avatar_image` (`avatar_image_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -246,7 +249,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Minh','m@gmail.com','123',NULL,'Nam','2023-11-08 15:42:45',NULL),(2,'Nam','m@gmail.com','123',NULL,'Nam','2023-11-08 15:53:40',NULL);
+INSERT INTO `user` VALUES (5,'Thùy Trang','hehe@gmail.com','e16c0ed9b03f69a9c24a7b888daff1df1c501e21298396cf70be3519e7eaf7a2','I am a rich girl','nu','2023-12-05 13:00:51',1),(6,'Quốc Nam','quocnam@gmail.com','e16c0ed9b03f69a9c24a7b888daff1df1c501e21298396cf70be3519e7eaf7a2','Ask me please!','nam','2023-12-06 12:14:52',2),(7,'Tuấn Minh','mlym8565@gmail.com','e16c0ed9b03f69a9c24a7b888daff1df1c501e21298396cf70be3519e7eaf7a2','Sleepy','nam','2023-12-08 22:16:55',1),(8,'Ngọc Nam','ngocnam20002@gmail.com','e16c0ed9b03f69a9c24a7b888daff1df1c501e21298396cf70be3519e7eaf7a2',NULL,'nam','2023-12-10 10:12:39',3);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -259,4 +262,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-08 16:48:47
+-- Dump completed on 2023-12-11 22:27:21
